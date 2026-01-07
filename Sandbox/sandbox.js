860eda70218700d5b0cb2c5c68590390ae2d004b
jacobcreation <iliganchesskid@gmail.com>
2026-01-07 19:04:48 +0800

    Update sandbox.js

--- Sandbox/sandbox.js
+++ Sandbox/sandbox.js
@@ -1,10 +1,30 @@
+import { files } from "./editor.js";
+
 export function runSandbox() {
   const frame = document.getElementById("frame");
+
+  const html = files.html || "";
+  const css = files.css || "";
+  const js = files.js || "";
+
   frame.srcdoc = `
-    <script>
-    console.log = (...a)=>parent.postMessage(a.join(" "), "*");
-    try { ${localStorage.js || ""} }
-    catch(e){ parent.postMessage("ERR:"+e, "*"); }
-    <\/script>
-  `;
+<!DOCTYPE html>
+<html>
+<head>
+  <style>${css}</style>
+</head>
+<body>
+  ${html}
+  <script>
+    const send = (msg) => parent.postMessage(msg, "*");
+    console.log = (...a) => send(a.join(" "));
+    try {
+      ${js}
+    } catch (e) {
+      send("ERR: " + e.message);
+    }
+  <\/script>
+</body>
+</html>
+`;
 }
