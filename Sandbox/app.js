a367e293c2f7afdd73606ff95c6ec2e12da2fc8f
jacobcreation <iliganchesskid@gmail.com>
2026-01-07 17:27:07 +0800

    Update app.js

--- Sandbox/app.js
+++ Sandbox/app.js
@@ -1,14 +1,39 @@
-import { files, renderFiles, saveFile } from "./editor.js";
+import { files, renderFiles, getCurrentFile, saveCurrentFile } from "./editor.js";
 import { runSandbox } from "./sandbox.js";
 import { askAI, fixWithAI, explainLastError } from "./ai.js";
 
-renderFiles();
+function init() {
+  console.log("Sandbox init");
 
-document.getElementById("run").onclick = runSandbox;
-document.getElementById("zip").onclick = () => saveFile("zip");
-document.getElementById("fix").onclick = fixWithAI;
-document.getElementById("explain").onclick = explainLastError;
+  renderFiles();
 
-document.getElementById("aiInput").onkeydown = e => {
-  if (e.key === "Enter") askAI(e.target.value);
-};
+  document.getElementById("run").addEventListener("click", () => {
+    saveCurrentFile();
+    runSandbox();
+  });
+
+  document.getElementById("zip").addEventListener("click", () => {
+    saveCurrentFile();
+    saveCurrentFile("zip");
+  });
+
+  document.getElementById("fix").addEventListener("click", fixWithAI);
+  document.getElementById("explain").addEventListener("click", explainLastError);
+
+  const aiInput = document.getElementById("aiInput");
+  aiInput.addEventListener("keydown", e => {
+    if (e.key === "Enter") {
+      askAI(aiInput.value);
+      aiInput.value = "";
+    }
+  });
+
+  // Load first file automatically
+  const first = Object.keys(files)[0];
+  document.getElementById("code").value = files[first];
+  document.getElementById("files").dataset.current = first;
+
+  runSandbox();
+}
+
+init();
