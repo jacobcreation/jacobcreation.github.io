82bb89856606efad21805f5054e3123604be9348
jacobcreation <iliganchesskid@gmail.com>
2026-01-07 19:10:04 +0800

    Update app.js

--- Sandbox/app.js
+++ Sandbox/app.js
@@ -1,6 +1,5 @@
-import { files, renderFiles, getCurrentFile, saveCurrentFile } from "./editor.js";
+import { renderFiles, saveCurrentFile } from "./editor.js";
 import { runSandbox } from "./sandbox.js";
-import { askAI, fixWithAI, explainLastError } from "./ai.js";
 
 function init() {
   console.log("Sandbox init");
@@ -12,26 +11,11 @@ function init() {
     runSandbox();
   });
 
-  document.getElementById("zip").addEventListener("click", () => {
-    saveCurrentFile();
-    saveCurrentFile("zip");
+  window.addEventListener("message", e => {
+    const c = document.getElementById("console");
+    c.textContent += e.data + "\n";
+    c.scrollTop = c.scrollHeight;
   });
-
-  document.getElementById("fix").addEventListener("click", fixWithAI);
-  document.getElementById("explain").addEventListener("click", explainLastError);
-
-  const aiInput = document.getElementById("aiInput");
-  aiInput.addEventListener("keydown", e => {
-    if (e.key === "Enter") {
-      askAI(aiInput.value);
-      aiInput.value = "";
-    }
-  });
-
-  // Load first file automatically
-  const first = Object.keys(files)[0];
-  document.getElementById("code").value = files[first];
-  document.getElementById("files").dataset.current = first;
 
   runSandbox();
 }
