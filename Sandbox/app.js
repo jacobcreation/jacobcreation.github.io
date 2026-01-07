e6ad9d560c895aa006f8248258d94ded98e40dbe
jacobcreation <iliganchesskid@gmail.com>
2026-01-07 19:23:41 +0800

    Update app.js

--- Sandbox/app.js
+++ Sandbox/app.js
@@ -1,5 +1,6 @@
-import { initEditor, saveCurrentFile } from "./editor.js";
+import { initEditor, saveCurrentFile, files } from "./editor.js";
 import { runSandbox } from "./sandbox.js";
+import { askAI } from "./ai.js";
 
 function init() {
   console.log("Sandbox init");
@@ -15,6 +16,15 @@ function init() {
     const c = document.getElementById("console");
     c.textContent += e.data + "\n";
     c.scrollTop = c.scrollHeight;
+  });
+
+  const aiInput = document.getElementById("aiInput");
+  aiInput.addEventListener("keydown", e => {
+    if (e.key === "Enter") {
+      saveCurrentFile();
+      askAI(aiInput.value, files);
+      aiInput.value = "";
+    }
   });
 
   runSandbox();
