47526e312001955d56002dc96a651962e1a6035a
jacobcreation <iliganchesskid@gmail.com>
2026-01-07 19:09:28 +0800

    Update sandbox.js

--- Sandbox/sandbox.js
+++ Sandbox/sandbox.js
@@ -2,28 +2,26 @@ import { files } from "./editor.js";
 
 export function runSandbox() {
   const frame = document.getElementById("frame");
-
-  const html = files.html || "";
-  const css = files.css || "";
-  const js = files.js || "";
+  const consoleEl = document.getElementById("console");
+  consoleEl.textContent = "";
 
   frame.srcdoc = `
 <!DOCTYPE html>
 <html>
 <head>
-  <style>${css}</style>
+<style>${files.css}</style>
 </head>
 <body>
-  ${html}
-  <script>
-    const send = (msg) => parent.postMessage(msg, "*");
-    console.log = (...a) => send(a.join(" "));
-    try {
-      ${js}
-    } catch (e) {
-      send("ERR: " + e.message);
-    }
-  <\/script>
+${files.html}
+<script>
+  const log = (...a) => parent.postMessage(a.join(" "), "*");
+  console.log = log;
+  try {
+    ${files.js}
+  } catch (e) {
+    log("ERR: " + e.message);
+  }
+<\/script>
 </body>
 </html>
 `;
